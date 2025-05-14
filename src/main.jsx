507c33c0325fee42b264fe1ad709776602a453e0
Roni Biswas <ronibiswas068@gmail.com>
2025-05-14 16:32:47 +0600

    added UI form for add coffee in AddCoffee component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./components/Home";
+import AddCoffee from "./components/AddCoffee";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "add-coffee",
+        Component: AddCoffee,
+      },
+    ],
   },
 ]);
 
